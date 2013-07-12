a5cf8627cd71beed9822aa53e3a064ba6fbff99d
Ubuntu <ubuntu@ip-172-31-34-129.us-west-2.compute.internal>
2013-07-12 16:07:15 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -2,11 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+fs = require('fs')
+fs.readFile('./index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  var snowman = new Buffer(data)
+  snowman.toString("utf-8",data);
 });
