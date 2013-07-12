e0bcea8019e5532174159343c7c83f98ac03c84e
Ubuntu <ubuntu@ip-172-31-34-129.us-west-2.compute.internal>
2013-07-12 16:35:54 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
-var http = require('http'),
-    fs = require('fs');
+var express = require('express');
+var fs = require('fs');
 
+var app = express.createServer(express.logger());
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
+app.get('/', function(request, response) {
+    var buffer = fs.readFileSync('index.html');
+    var text = buffer.toString();
+    response.send(text);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
