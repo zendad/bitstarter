ad457f2c661788df4d851a6e6b9d8d51646ffa70
Ubuntu <ubuntu@ip-172-31-34-129.us-west-2.compute.internal>
2013-07-15 10:16:09 +0200

    Added checks.json

--- grader.js
+++ grader.js
@@ -65,7 +65,8 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
+        .option('-u, --url <html_file>', 'Link to a url to index.html')
+	.parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
